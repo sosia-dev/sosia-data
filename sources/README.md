263188e26cea4fcccb5050b845fb37e5bfc4d0f0
Michael E. Rose <Michael.Ernst.Rose@gmail.com>
2020-10-18 23:18:59 +0200

    Generate source definition

---
+++ sources/README.md
@@ -0,0 +1,4 @@
+Execute [generate_sources_definition.py](generate_sources_definition.py) follwing Scopus' update of Source information and store resulting data here.
+
+- [field_sources_list.csv](field_sources_list.csv): Assigment of sources to fields
+- [sources_names.csv](sources_names.csv): Names of sources
---
+++ sources/generate_sources_definition.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python3
+# Authors:  Michael E. Rose <michael.ernst.rose@gmail.com>
+#           Stefano H. Baruffaldi <ste.baruffaldi@gmail.com>
+"""Compiles up-to-date source information to be downloaded by sosia."""
+
+from zipfile import ZipFile
+from io import BytesIO
+
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+SOURCES_FNAME = r"CiteScore 2011-2019 new methodology - June 2020.xlsb"
+URL_CONTENT = "https://www.elsevier.com/solutions/scopus/how-scopus-works/content"
+URL_SOURCES = "https://elsevier.com/?a=734751"
+
+
+def create_fields_sources_list():
+    """Download Scopus files with information on covered sources and create
+    one list of all sources with ids and one with field information.
+    """
+    # Set up
+    rename = {
+        "All Science Journal Classification Codes (ASJC)": "asjc",
+        "Scopus ASJC Code (Sub-subject Area)": "asjc",
+        "ASJC code": "asjc",
+        "Source Type": "type",
+        "ASJC Code": "asjc",
+        "Type": "type",
+        "Sourcerecord id": "source_id",
+        "Sourcerecord ID": "source_id",
+        "Scopus Source ID": "source_id",
+        "Scopus SourceID": "source_id",
+        "Scopus Source ID": "source_id",
+        "Title": "title",
+        "Source title": "title",
+        "Source Title (Medline-sourced journals are indicated in Green)": "title",
+        "Conference Title": "title"
+    }
+    keeps = list(set(rename.values()))
+
+    # Get Information from Scopus Sources list
+    archive = ZipFile(BytesIO(requests.get(URL_SOURCES).content))
+    sources = pd.read_excel(archive.read(SOURCES_FNAME), sheet_name=None,
+                            engine='pyxlsb')
+    _drop_sheets(sources, ["About CiteScore", "ASJC codes"])
+    dfs = [df.rename(columns=rename)[keeps].dropna() for df in sources.values()]
+    out = pd.concat(dfs).drop_duplicates()
+
+    # Add information from list of external publication titles
+    external = pd.read_excel(_get_source_title_url(), sheet_name=None)
+    _drop_sheets(external, ["More info Medline", "ASJC classification codes"])
+
+    for df in external.values():
+        _update_dict(rename, df.columns, "source title", "title")
+        if "Source Type" not in df.columns:
+            df["type"] = "conference proceedings"
+        subset = df.rename(columns=rename)[keeps].dropna()
+        subset["asjc"] = subset["asjc"].astype(str).apply(_clean).str.split()
+        subset = (subset.set_index(["source_id", "title", "type"])
+                        .asjc.apply(pd.Series)
+                        .stack()
+                        .rename("asjc")
+                        .reset_index()
+                        .drop("level_3", axis=1))
+        out = pd.concat([out, subset], sort=True)
+
+    # Write list of names
+    order = ["source_id", "title"]
+    names = out[order].drop_duplicates().sort_values("source_id")
+    names.to_csv("./sources_names.csv", index=False)
+
+    # Write list of fields by source
+    out["type"] = out["type"].str.lower().str.strip()
+    out["asjc"] = out["asjc"].astype(int)
+    out.drop("title", axis=1).to_csv("./field_sources_list.csv", index=False)
+
+
+def _clean(x):
+    """Auxiliary function to clean a string Series."""
+    return x.replace(";", " ").replace(",", " ").replace("  ", " ").strip()
+
+
+def _drop_sheets(sheets, drops):
+    """Auxiliary function to drop sheets from an Excel DataFrame."""
+    for drop in drops:
+        try:
+            sheets.pop(drop)
+        except KeyError:
+            continue
+
+
+def _get_source_title_url():
+    """Extract the link to the most recent Scopus sources list."""
+    resp = requests.get(URL_CONTENT)
+    soup = BeautifulSoup(resp.text, "lxml")
+    try:
+        return soup.find("a", {"title": "source list"})["href"]
+    except AttributeError:
+        raise ValueError("Link to sources list not found.")
+
+
+def _update_dict(d, lst, key, replacement):
+    """Auxiliary function to add keys to a dictionary if a given string is
+    included in the key.
+    """
+    for c in lst:
+        if c.lower().startswith(key):
+            d[c] = replacement
+
+
+if __name__ == '__main__':
+    create_fields_sources_list()
